8c5571a0916601cc3d363e7c7760fee9510c3d4d
Kibuzzz <66959531+Kibuzzz@users.noreply.github.com>
2024-09-12 18:28:02 +0300

    basic dev2

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"errors"
+	"strings"
+	"unicode"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -17,6 +23,49 @@ package main
 
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
+
+var (
+	ErrorInvalidString = errors.New("некорректная строка")
+)
+
+func isValid(runes []rune) bool {
+	for i := 0; i < len(runes)-1; i++ {
+		cur := runes[i]
+		next := runes[i+1]
+		if unicode.IsDigit(cur) && unicode.IsDigit(next) {
+			return false
+		}
+	}
+	return true
+}
+
+func Unpack(input string) (string, error) {
+	runes := []rune(input)
+
+	if len(runes) == 0 {
+		return "", nil
+	}
+
+	if !isValid(runes) {
+		return "", ErrorInvalidString
+	}
+
+	l := len(runes)
+	var result string
+	for i := 0; i < l-1; i++ {
+		cur := runes[i]
+		next := runes[i+1]
+		if unicode.IsLetter(cur) && unicode.IsDigit(next) {
+			n := int(next - '0')
+			result += strings.Repeat(string(cur), n)
+			i++ // потому что уже посмотрели - это цифра
+		} else if unicode.IsLetter(cur) {
+			result += string(cur)
+		}
+	}
+	result += string(runes[l-1])
+	return result, nil
+}
 
 func main() {
 
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// "a4bc2d5e" => "aaaabccddddde"
+// "abcd" => "abcd"
+// "45" => "" (некорректная строка)
+// "" => ""
+
+func TestUnpack(t *testing.T) {
+	tests := []struct {
+		input  string
+		wanted string
+		err    error
+	}{
+		{
+			"a4bc2d5e",
+			"aaaabccddddde",
+			nil,
+		},
+		{
+			"abcd",
+			"abcd",
+			nil,
+		},
+		{
+			"45",
+			"",
+			ErrorInvalidString,
+		},
+		{
+			"",
+			"",
+			nil,
+		},
+	}
+	for _, test := range tests {
+		result, err := Unpack(test.input)
+		assert.ErrorIs(t, err, test.err, "wrong error")
+		assert.EqualValues(t, test.wanted, result, "wrong result")
+	}
+}
