9c56e9d2462e4bcbb6f831bd287b5a443281d640
Kibuzzz <66959531+Kibuzzz@users.noreply.github.com>
2024-09-17 17:48:28 +0300

    dev05 grep

--- develop/dev05/task.go
+++ develop/dev05/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита grep ===
 
@@ -18,6 +27,155 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type flags struct {
+	after      int
+	before     int
+	context    int
+	count      bool
+	ignoreCase bool
+	invert     bool
+	fixed      bool
+	lineNum    bool
+}
 
+func main() {
+	after := flag.Int(`A`, 0, `"after" печатать +N строк после совпадения`)
+	before := flag.Int(`B`, 0, `-B - "before" печатать +N строк до совпадения`)
+	context := flag.Int(`C`, 0, `-C - "context" (A+B) печатать ±N строк вокруг совпадения`)
+	count := flag.Bool(`c`, false, `-c - "count" (количество строк)`)
+	ignoreCase := flag.Bool(`i`, false, `-i - "ignore-case" (игнорировать регистр)`)
+	invert := flag.Bool(`v`, false, `-v - "invert" (вместо совпадения, исключать)`)
+	fixed := flag.Bool(`F`, false, `-F - "fixed", точное совпадение со строкой, не паттерн`)
+	lineNum := flag.Bool(`n`, false, `-n - "line num", печатать номер строки`)
+
+	flag.Parse()
+
+	flags := flags{
+		after:      *after,
+		before:     *before,
+		context:    *context,
+		count:      *count,
+		ignoreCase: *ignoreCase,
+		invert:     *invert,
+		fixed:      *fixed,
+		lineNum:    *lineNum,
+	}
+
+	if *after < 0 {
+		log.Fatal("after flag should be >= 0")
+	}
+
+	// Чтение аргументов командной строки
+	pattern := flag.Arg(0)
+	fileName := flag.Arg(1)
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("failed to open file %s\n", err.Error())
+	}
+	file.Close()
+
+	lines := []string{}
+	Grep(lines, pattern, flags)
+}
+
+func Grep(lines []string, pattern string, flags flags) []string {
+
+	var counter int
+	var output []string
+	linesInOutput := make(map[int]bool) // чтобы не допустить повторного добавления уже довабленной строки
+
+	if flags.context != 0 {
+		flags.after = flags.context
+		flags.before = flags.context
+	}
+
+	for lineIndex, line := range lines {
+
+		// Для проверки есть ли совпадение в строке
+		match := findSubString(line, pattern, flags)
+
+		var appendLine bool
+
+		switch {
+		case flags.invert && !match:
+			appendLine = true
+		case !flags.invert && match:
+			appendLine = true
+		}
+
+		if appendLine {
+			counter++
+			switch {
+			case flags.context > 0:
+				fallthrough
+			case flags.before > 0:
+				rightBarier := lineIndex
+				leftBarier := lineIndex - flags.before
+				if leftBarier < 0 {
+					leftBarier = 0
+				}
+				output = appendLines(lines, output, linesInOutput, leftBarier, rightBarier, flags)
+				fallthrough
+			case flags.after > 0:
+				leftBarier := lineIndex
+				rightBarier := lineIndex + flags.after
+				if rightBarier >= len(lines) {
+					rightBarier = len(lines) - 1
+				}
+				output = appendLines(lines, output, linesInOutput, leftBarier, rightBarier, flags)
+			default:
+				if flags.lineNum {
+					line = fmt.Sprintf("%d:%s", lineIndex+1, line)
+				}
+				output = append(output, line)
+			}
+		}
+	}
+
+	if flags.count {
+		numLines := strconv.Itoa(counter)
+		output = []string{numLines}
+	}
+
+	return output
+}
+
+func appendLines(lines []string, output []string, duplicates map[int]bool, leftBarier int, rightBarier int, flags flags) []string {
+	for i := leftBarier; i <= rightBarier; i++ {
+		line := lines[i]
+		if flags.lineNum {
+			line = fmt.Sprintf("%d:%s", i+1, line)
+		}
+		if _, duplicate := duplicates[i]; !duplicate {
+			output = append(output, line)
+			duplicates[i] = true
+		}
+	}
+	return output
+}
+
+func findSubString(line string, pattern string, flags flags) bool {
+	if flags.fixed {
+
+		var words []string
+
+		if flags.ignoreCase {
+			words = strings.Split(strings.ToLower(line), " ")
+			pattern = strings.ToLower(pattern)
+		} else {
+			words = strings.Split(line, " ")
+		}
+
+		for _, word := range words {
+			if word == pattern {
+				return true
+			}
+		}
+		return false
+	}
+	if flags.ignoreCase {
+		return strings.Contains(strings.ToLower(line), strings.ToLower(pattern))
+	}
+	return strings.Contains(line, pattern)
 }
--- develop/dev05/task_test.go
+++ develop/dev05/task_test.go
@@ -1,1 +1,154 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGrep(t *testing.T) {
+	tests := []struct {
+		name    string
+		lines   []string
+		pattern string
+		flags   flags
+		want    []string
+	}{
+		{
+			name:    "Test count",
+			lines:   []string{"1112111", "1"},
+			pattern: "2",
+			flags: flags{
+				count: true,
+			},
+			want: []string{"1"},
+		},
+		{
+			name:    "Test ignoreCase",
+			lines:   []string{"A", "B", "C", "a"},
+			pattern: "A",
+			flags: flags{
+				ignoreCase: true,
+			},
+			want: []string{"A", "a"},
+		},
+		{
+			name:    "Test lineNum",
+			lines:   []string{"A", "B", "C", "a"},
+			pattern: "A",
+			flags: flags{
+				lineNum: true,
+			},
+			want: []string{"1:A"},
+		},
+		{
+			name:    "Test invert",
+			lines:   []string{"Ab", "Bb", "C", "a"},
+			pattern: "b",
+			flags: flags{
+				invert: true,
+			},
+			want: []string{"C", "a"},
+		},
+		{
+			name:    "Test invert and count",
+			lines:   []string{"Ab", "Bb", "C", "a"},
+			pattern: "b",
+			flags: flags{
+				invert: true,
+				count:  true,
+			},
+			want: []string{"2"},
+		},
+		{
+			name:    "Test fixed",
+			lines:   []string{"abcd", "a"},
+			pattern: "a",
+			flags: flags{
+				fixed: true,
+			},
+			want: []string{"a"},
+		},
+		{
+			name:    "Test fixed and ignore case",
+			lines:   []string{"Abcd", "A", "abcd", "a"},
+			pattern: "a",
+			flags: flags{
+				fixed:      true,
+				ignoreCase: true,
+			},
+			want: []string{"A", "a"},
+		},
+		{
+			name:    "Test fixed and ignore case",
+			lines:   []string{"Abcd", "A", "abcd", "a"},
+			pattern: "a",
+			flags: flags{
+				fixed:      true,
+				ignoreCase: true,
+			},
+			want: []string{"A", "a"},
+		},
+		{
+			name:    "Test -A",
+			lines:   []string{"a", "1"},
+			pattern: "a",
+			flags: flags{
+				after: 1,
+			},
+			want: []string{"a", "1"},
+		},
+		{
+			name:    "Test -A строк после больше чем есть",
+			lines:   []string{"a", "1"},
+			pattern: "a",
+			flags: flags{
+				after: 5,
+			},
+			want: []string{"a", "1"},
+		},
+		{
+			name:    "Test -A проверка на дубликаты",
+			lines:   []string{"a", "a", "1", "b"},
+			pattern: "a",
+			flags: flags{
+				after: 1,
+			},
+			want: []string{"a", "a", "1"},
+		},
+		{
+			name:    "Test -A -n",
+			lines:   []string{"a", "a", "1", "b"},
+			pattern: "a",
+			flags: flags{
+				after:   1,
+				lineNum: true,
+			},
+			want: []string{"1:a", "2:a", "3:1"},
+		},
+		{
+			name:    "Test -B 1",
+			lines:   []string{"1", "b", "a"},
+			pattern: "b",
+			flags: flags{
+				before: 1,
+			},
+			want: []string{"1", "b"},
+		},
+		{
+			name:    "Test -C 1",
+			lines:   []string{"left2", "left1", "a", "right1", "right2"},
+			pattern: "a",
+			flags: flags{
+				context: 1,
+			},
+			want: []string{"left1", "a", "right1"},
+		},
+	}
+	for _, test := range tests {
+		get := Grep(test.lines, test.pattern, test.flags)
+		assert.Equal(t, test.want, get, test.name)
+		fmt.Printf("%s DONE\n", test.name)
+	}
+}
