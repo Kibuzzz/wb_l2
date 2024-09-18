82db61ff70666c78dcbaa054d184d9169754d5b9
Kibuzzz <66959531+Kibuzzz@users.noreply.github.com>
2024-09-18 16:46:32 +0300

    telnet dev10

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+)
+
 /*
 === Утилита telnet ===
 
@@ -11,10 +26,99 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 После подключения STDIN программы должен записываться в сокет, а данные полученные и сокета должны выводиться в STDOUT
 Опционально в программу можно передать таймаут на подключение к серверу (через аргумент --timeout, по умолчанию 10s).
 
-При нажатии Ctrl+D программа должна закрывать сокет и завершаться. Если сокет закрывается со стороны сервера, программа должна также завершаться.
+При нажатии Ctrl+D программа должна закрывать сокет и завершаться. Если сокет закрывается со стороны сервера,
+программа должна также завершаться.
+
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
 
 func main() {
+	timeout := flag.Int("timeout", 10, "sets timeout")
 
+	flag.Parse()
+
+	if len(flag.Args()) != 2 {
+		log.Fatal("two arguments needed: host and port")
+	}
+
+	args := flag.Args()
+	host, port := args[0], args[1]
+
+	_, err := strconv.Atoi(port)
+	if err != nil {
+		log.Fatal("port should be a number")
+	}
+	addr := fmt.Sprintf("%s:%s", host, port)
+
+	input := os.Stdin
+	output := os.Stdout
+
+	err = telnet(addr, *timeout, input, output)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func telnet(addr string, timeout int, input io.Reader, output io.Writer) (err error) {
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	conn, err := net.DialTimeout("tcp", addr, time.Second*time.Duration(timeout))
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	errCh := make(chan error, 1)
+
+	go func() {
+		defer wg.Done()
+		scanner := bufio.NewScanner(input)
+		for scanner.Scan() {
+			line := scanner.Bytes()
+			_, err = conn.Write(line)
+			if err != nil {
+				errCh <- err
+				return
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			errCh <- err
+		}
+		close(errCh)
+	}()
+
+	go func() {
+		defer wg.Done()
+		for {
+			buff := make([]byte, 1024)
+			n, err := conn.Read(buff)
+			if err != nil {
+				errCh <- err
+				return
+			}
+			_, err = output.Write(buff[:n])
+			if err != nil {
+				errCh <- err
+				return
+			}
+		}
+	}()
+
+	select {
+	case err := <-errCh:
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+	case <-sigCh:
+		fmt.Println("Program closed")
+		os.Exit(0)
+	}
+
+	wg.Wait()
+	return nil
 }
--- develop/dev10/task_test.go
+++ develop/dev10/task_test.go
@@ -1,1 +1,91 @@
 package main
+
+import (
+	"bytes"
+	"log"
+	"net"
+	"sync"
+	"testing"
+)
+
+var (
+	testMsg1 = "msg to server"
+	testMsg2 = "msg from server"
+)
+
+func newServer(input *bytes.Buffer, output []byte) testServer {
+	return testServer{
+		input:  input,
+		output: output,
+	}
+}
+
+type testServer struct {
+	input  *bytes.Buffer
+	output []byte
+}
+
+func (ts *testServer) Run(addr string) error {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	conn, err := listener.Accept()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go ts.readConn(conn, wg)
+	go ts.writeConn(conn, wg)
+	wg.Wait()
+	return nil
+}
+
+func TestTelnet(t *testing.T) {
+	addr := "localhost:8080"
+
+	serverInput := &bytes.Buffer{}
+	serverOutput := []byte(testMsg2)
+
+	server := newServer(serverInput, serverOutput)
+	go server.Run(addr)
+
+	telnetInput := bytes.NewBufferString(testMsg1)
+	telnetOutput := &bytes.Buffer{}
+
+	telnet(addr, telnetInput, telnetOutput)
+
+	// Проверка, что до сервера доходят правильные сообщения
+	if server.input.String() != testMsg1 {
+		t.Errorf("wrong server input: got %q, wanted %q", server.input.String(), testMsg1)
+	}
+
+	if telnetOutput.String() != string(server.output) {
+		t.Errorf("wrong telnet output: got %q, wanted %q", telnetOutput.String(), string(server.output))
+	}
+}
+
+func (ts *testServer) readConn(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// Read data from the client
+	bytes := make([]byte, 1024)
+	n, err := conn.Read(bytes)
+	if err != nil {
+		return
+	}
+	_, err = ts.input.Write(bytes[:n])
+	if err != nil {
+		log.Fatal("failed to write to server input", err)
+	}
+}
+
+func (ts *testServer) writeConn(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	_, err := conn.Write([]byte(testMsg2))
+	if err != nil {
+		return
+	}
+}
