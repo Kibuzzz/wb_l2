2b2e8614f07ba367a5317aa8a3c48be8aada454e
Kibuzzz <66959531+Kibuzzz@users.noreply.github.com>
2024-09-18 11:47:20 +0300

    wget dev09

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
 /*
 === Утилита wget ===
 
@@ -8,6 +16,42 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func wget(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
+	filename := filepath.Base(url)
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	_, err = file.Write(body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("URL required")
+		return
+	}
+
+	url := os.Args[1]
+
+	err := wget(url)
+	if err != nil {
+		fmt.Printf("Error downloading file: %s\n", err.Error())
+		return
+	}
+	fmt.Println("File downloaded")
 }
--- develop/dev09/task_test.go
+++ develop/dev09/task_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestDownload(t *testing.T) {
+	filename := "index.html"
+	url := "https://www.youtube.com/" + filename
+
+	err := wget(url)
+	if err != nil {
+		t.Errorf("Ошибка при скачивании файла: %s", err.Error())
+	}
+
+	_, err = os.Stat(filename)
+	if os.IsNotExist(err) {
+		t.Errorf("Файл не был создан")
+	}
+
+	err = os.Remove(filename)
+	if err != nil {
+		t.Errorf("Ошибка при удалении файла: %s", err.Error())
+	}
+}
